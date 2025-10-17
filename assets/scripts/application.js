a5cdf646eed3bfceab5ed1006b3027a9a2e7514f
Iulian Cristian Grigoriță <iulian.cristian.grigorita@gmail.com>
2025-10-17 18:43:22 +0300

    Update scripts

--- assets/scripts/application.js
+++ assets/scripts/application.js
@@ -1,1 +1,4 @@
-(function(window, document, undefined) { })(window, document);
+(function(window, document, undefined) {
+    const initializedApplicationInitializedClass = "application-initialized";
+    document.body.classList.add(initializedApplicationInitializedClass);
+})(window, document);
