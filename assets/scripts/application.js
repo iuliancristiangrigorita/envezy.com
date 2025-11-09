360322dc3e8b6545fcfdf58a605783cc896a03e8
Iulian Cristian Grigoriță <iulian.cristian.grigorita@gmail.com>
2025-11-09 22:31:39 +0200

    Update scripts

--- assets/scripts/application.js
+++ assets/scripts/application.js
@@ -1,6 +1,4 @@
 (function(window, document, undefined) {
-    let applicationRootId = 'application-root';
     let applicationRootClass = 'application-root';
-    document.body.id = applicationRootId;
     document.body.classList.add(applicationRootClass);
 })(window, document);
